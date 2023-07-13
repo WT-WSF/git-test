3272158cca198c210c569816bedff550c8b301b0
wsf <1604543063@qq.com>
2023-07-13 15:41:06 +0800

    first commit

---
+++ src/main/java/com/test/GitTest.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * @author wsf
+ * @date 2023/7/13
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,Git");
+    }
+}
